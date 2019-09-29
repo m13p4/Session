a9dddda76243868d679d4e399e65b10b84fc9605
m13p4 <pavel@wtnet.de>
2019-09-29 19:39:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-/*
- * Session Server.
+/* Session Server.
  * 
  * @author Meliantchenkov Pavel
  * @version 1.0
  */
-
 (function(___)
 {
     const VERSION = "1.0.not_tested";
@@ -37,129 +35,121 @@
         else if(res && !withoutExp) 
             _sessions[sid].exp = now + _sessions[sid]._exp;
         
-        console.log("session check", res);
-        
         return res;
     }
     
-    function handleMessage(_CONF, _sessions, socket, dataStr)
+    function handleMessage(_CONF, _sessions, socket, dataArr)
     {
-        try
+        for(let i = 0; i < dataArr.length; i++)
         {
-            var dataArr = dataStr.split(_sep), i = 0;
-            
-            for(; i < dataArr.length; i++)
+            let data = dataArr[i].trim();
+
+            if(data.length > 0)
             {
-                let data = dataArr[i].trim();
-                
-                if(data.length > 0)
+                try
                 {
-                    try
+                    data = JSON.parse(data);
+
+                    if(data instanceof Array && data.length > 0)
                     {
-                        data = JSON.parse(data);
-                        
-                        if(data instanceof Array && data.length > 0)
+                        let id   = data[0];
+                        let type = data[1] || "";
+                        let hash = data[2] || "";
+                        let sess = data[3] || "";
+
+                        let res  = [id];
+
+                        if(type === "new" && hash.length > 0)
                         {
-                            let id   = data[0];
-                            let type = data[1] || "";
-                            let hash = data[2] || "";
-                            let sess = data[3] || "";
+                            res.push("new");
 
-                            let res  = [id];
+                            let expTime   = Number.isInteger(sess) && sess > 0 ? sess : _CONF.expTime;
+                            let keyLength = Number.isInteger(data[4]) && data[4] > 0 ? data[4] : _CONF.defaultKeyLength;
+                            let base; if(Number.isInteger(data[5])) base = data[5];
 
-                            if(type === "new" && hash.length > 0)
-                            {
-                                res.push("new");
+                            if(keyLength > _CONF.largestKeyLength) keyLength = _CONF.largestKeyLength;
 
-                                let expTime   = Number.isInteger(sess) && sess > 0 ? sess : _CONF.expTime;
-                                let keyLength = Number.isInteger(data[4]) && data[4] > 0 ? data[4] : _CONF.defaultKeyLength;
-                                let base; if(Number.isInteger(data[5])) base = data[5];
+                            let newSession = {
+                                key:  _.getRandomString(keyLength, base),
+                                hash: hash,
+                                exp:  Date.now() + expTime,
 
-                                if(keyLength > _CONF.largestKeyLength) keyLength = _CONF.largestKeyLength;
+                                _exp: expTime,
+                                _var: {}
+                            };
 
-                                let newSession = {
-                                    key:  _.getRandomString(keyLength, base),
-                                    hash: hash,
-                                    exp:  Date.now() + expTime,
+                            while(_sessions[newSession.key])
+                                newSession.key = _.getRandomString(keyLength, base);
 
-                                    _exp: expTime,
-                                    _var: {}
-                                };
-
-                                while(_sessions[newSession.key])
-                                    newSession.key = _.getRandomString(keyLength, base);
-
-                                _sessions[newSession.key] = newSession;
-                                res.push(newSession.key);
-                            }
-                            else if(type === "check" && hash.length > 0 && sess.length > 0)
-                            {
-                                res.push("check");
-                                res.push(checkSession(_sessions, sess, hash));
-                            }
-                            else if(type === "close" && hash.length > 0 && sess.length > 0)
-                            {
-                                res.push("close");
-
-                                if(checkSession(_sessions, sess, hash, true))
-                                {
-                                    _sessions[sess] = null;
-                                    delete _sessions[sess];
-
-                                    res.push(true);
-                                }
-                                else res.push(false);
-                            }
-                            else if(type === "set" && hash.length > 0 && sess.length > 0)
-                            {
-                                res.push("set");
-                                res.push(false);
-
-                                if(checkSession(_sessions, sess, hash))
-                                {
-                                    let key = 4 in data ? data[4] : null;
-                                    let val = 5 in data ? data[5] : null;
-
-                                    if(typeof key !== "string") key = JSON.stringify(key);
-
-                                    _sessions[sess]._var[key] = val;
-                                    res[2] = true;
-                                }
-                            }
-                            else if(type === "get" && hash.length > 0 && sess.length > 0)
-                            {
-                                res.push("get");
-
-                                let key = 4 in data ? data[4] : null;
-                                let val = null;
-
-                                if(checkSession(_sessions, sess, hash))
-                                {
-                                    if(typeof key !== "string") key = JSON.stringify(key);
-
-                                    if(key && _sessions[sess]._var[key])
-                                        val = _sessions[sess]._var[key];
-                                }
-
-                                res.push(key);
-                                res.push(val);
-                            }
-                            else if(type === "ping") res.push("PING");
-                            else
-                            {
-                                res.push("err");
-                                res.push("incomprehensible request");
-                                res.push(data);
-                            }
-
-                            res.length > 0 && socket.write(JSON.stringify(res)+_sep);
+                            _sessions[newSession.key] = newSession;
+                            res.push(newSession.key);
                         }
+                        else if(type === "check" && hash.length > 0 && sess.length > 0)
+                        {
+                            res.push("check");
+                            res.push(checkSession(_sessions, sess, hash));
+                        }
+                        else if(type === "close" && hash.length > 0 && sess.length > 0)
+                        {
+                            res.push("close");
+
+                            if(checkSession(_sessions, sess, hash, true))
+                            {
+                                _sessions[sess] = null;
+                                delete _sessions[sess];
+
+                                res.push(true);
+                            }
+                            else res.push(false);
+                        }
+                        else if(type === "set" && hash.length > 0 && sess.length > 0)
+                        {
+                            res.push("set");
+                            res.push(false);
+
+                            if(checkSession(_sessions, sess, hash))
+                            {
+                                let key = 4 in data ? data[4] : null;
+                                let val = 5 in data ? data[5] : null;
+
+                                if(typeof key !== "string") key = JSON.stringify(key);
+
+                                _sessions[sess]._var[key] = val;
+                                res[2] = true;
+                            }
+                        }
+                        else if(type === "get" && hash.length > 0 && sess.length > 0)
+                        {
+                            res.push("get");
+
+                            let key = 4 in data ? data[4] : null;
+                            let val = null;
+
+                            if(checkSession(_sessions, sess, hash))
+                            {
+                                if(typeof key !== "string") key = JSON.stringify(key);
+
+                                if(key && _sessions[sess]._var[key])
+                                    val = _sessions[sess]._var[key];
+                            }
+
+                            res.push(key);
+                            res.push(val);
+                        }
+                        else if(type === "ping") res.push("PING");
+                        else
+                        {
+                            res.push("err");
+                            res.push("incomprehensible request");
+                            res.push(data);
+                        }
+
+                        res.length > 0 && socket.write(JSON.stringify(res)+_sep);
                     }
-                    catch(e){ /* empty */ }
                 }
+                catch(e){ /* empty */ }
             }
         }
-        catch(e){ /* empty */ }
     }
     
     function _run(_CONF, _sessions, _sockets)
@@ -182,8 +172,10 @@
                     
                     if(dataStr.indexOf(_sep) > -1)
                     {
-                        handleMessage(_CONF, _sessions, socket, dataStr);
-                        dataStr = "";
+                        let dataArr = dataStr.split(_sep);
+                        dataStr = dataArr.pop();
+                        
+                        handleMessage(_CONF, _sessions, socket, dataArr);
                     }
                 }
                 catch(e) { /* empty */ }
