d6011a8e340d2a95da36e25e15c6929127cf159c
m13p4 <pavel@wtnet.de>
2019-09-29 16:58:09 +0200

    Create session.js

---
+++ session.js
@@ -0,0 +1,11 @@
+/*
+ * Session server and client
+ * 
+ * @author Meliantchenkov Pavel
+ * @version 1.0
+ */
+
+module.exports = {
+    Server: require("./server"),
+    Client: require("./client")
+};
